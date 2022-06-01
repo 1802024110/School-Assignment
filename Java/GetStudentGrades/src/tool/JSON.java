65d617c21e0dfd84e25093f0fdf3a589c2cc34fd
NoDream <1802024110@qq.com>
2022-06-01 16:08:32 +0800

    添加maven

--- Java/GetStudentGrades/src/tool/JSON.java
+++ Java/GetStudentGrades/src/tool/JSON.java
@@ -3,6 +3,6 @@ package tool;
 public class JSON {
     // TODO: 实现JSON解析
     public static void main(String[] args) {
-//        JSONArray jsonArray = new JSONArray(jsonData);
+        JSONArray jsonArray = new JSONArray(jsonData);
     }
 }
