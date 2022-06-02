b878f464953f284a3e00671305f02929c0c02dd8
NoDream <1802024110@qq.com>
2022-06-02 14:15:39 +0800

    更改JSON

--- Java/GetStudentGrades/src/tool/JSON.java
+++ Java/GetStudentGrades/src/tool/JSON.java
@@ -1,8 +1,10 @@
 package tool;
 
+import org.json.JSONArray;
+
 public class JSON {
     // TODO: 实现JSON解析
     public static void main(String[] args) {
-        JSONArray jsonArray = new JSONArray(jsonData);
+        JSONArray jsonArray = new JSONArray();
     }
 }
