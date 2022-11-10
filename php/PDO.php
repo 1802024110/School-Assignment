46a60454662f19164397295de4aaea544df57e17
noDream <1802024110@qq.com>
2022-11-10 22:41:16 +0800

    gx

--- php/PDO.php
+++ php/PDO.php
@@ -1,12 +1,16 @@
 <?
+// 因为数据库有ssl验证连不上
 $hostname = "ap-northeast.connect.psdb.cloud";
 $username = "1bdd05hkapbe1crd3iok";
 $password = "pscale_pw_bKGpeYnF63N4zUsUcZpnd08IrGjolI63UbQbZ5sYG2h";
 $database = "nodream";
 $ca = "用到的代码\cacert.pem";
 
+$options = array_merge($options,array(
+  PDO::MYSQL_ATTR_SSL_CA =>$ca
+));
 try {
-  $conn = new PDO("mysql:host=$hostname;", $username, $password);
+  $conn = new PDO("mysql:host=$hostname;", $username, $password,$options);
   echo "连接成功"; 
 }
 catch(PDOException $e)
--- php/mysqli.php
+++ php/mysqli.php
@@ -6,21 +6,65 @@ $mysqli->real_connect('ap-northeast.connect.psdb.cloud', '1bdd05hkapbe1crd3iok',
 $mysqli->close();
  */
 
-$hostname = "ap-northeast.connect.psdb.cloud";
-$username = "1bdd05hkapbe1crd3iok";
-$password = "pscale_pw_bKGpeYnF63N4zUsUcZpnd08IrGjolI63UbQbZ5sYG2h";
-$database = "nodream";
-$ca = "用到的代码\cacert.pem";
+$hostname = "127.0.0.1";
+$username = "root";
+$password = "123456";
+$database = "phpLean";
 
 $mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, "用到的代码\cacert.pem", NULL, NULL);
-$mysqli->real_connect('ap-northeast.connect.psdb.cloud', '1bdd05hkapbe1crd3iok', 'pscale_pw_bKGpeYnF63N4zUsUcZpnd08IrGjolI63UbQbZ5sYG2h', 'nodream');
+$mysqli->real_connect($hostname, $username,$password,$database );
 
 // 检测连接
 if (!$mysqli) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "连接成功";
+echo "连接成功\n";
 
-$mysqli->close();
-?>
+// 执行SQL语句
+$sql1 = "SELECT * FROM phpLean.employee;";
+if($mysqli->query($sql1)){
+  echo "执行成功\n";
+}else{
+  echo "执行失败:$mysqli->error\n";
+}
+
+// 执行多条语句
+$sql2 = <<<SQL2
+insert into phpLean.`Employee` (`num`, `name`, `sex`) values ('wG7', '任子轩', '女');
+insert into phpLean.`Employee` (`num`, `name`, `sex`) values ('Nk2C', '潘烨伟', '女');
+insert into phpLean.`Employee` (`num`, `name`, `sex`) values ('cZ', '卢智辉', '男');
+SQL2;
+// if($mysqli->multi_query($sql2)){
+//   echo "新记录插入成功";
+// }else{
+//   echo "Error: " .$mysqli->error;
+// }
+
+// 预处理可以给SQL语句绑定变量
+$sql3 = "insert into phpLean.`Employee` (`num`, `name`, `sex`) values (?, ?, ?);";
+$num = "asdfsdf";
+$name ="老王";
+$sex="男";
+if($stmt = $mysqli->prepare($sql3)){
+  // 绑定变量，这里只能引用变量，不能直接传入值
+  // s表示字符串，i表示int,d表示float,b表示blob二进制存储对象
+  $stmt->bind_param("sss",$num,$name,$sex);
+  // 执行
+  if($stmt->execute()){
+    echo "预处理插入成功\n";
+  }else{
+    echo "Error: " .$mysqli->error;
+  }
+}
+
+// 读取数据
+$sql4 = "SELECT * FROM phpLean.employee;";
+if(($result = $mysqli->query($sql4))&&$result>0){
+  // fetch_assoc是迭代器
+  while($row = $result->fetch_assoc()){
+    echo "num:{$row["num"]},name:{$row["name"]},sex:{$row["sex"]}";
+  }
+}
+
+// $mysqli->close();
+// ?>
