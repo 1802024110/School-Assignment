3b48001a33dd5f488f56fab7fa79ee2f38a9b4dc
NoDream <1802024110@qq.com>
2022-11-10 09:46:51 +0800

    更新

--- php/Session.php
+++ php/Session.php
@@ -1,7 +1,7 @@
 <?
 // session_start()必须位于html标签前
-
-// 此函数会为用户赋予一个随机的session
+// session是由php随机生成的加密数字
+// session信息是在服务器，但session id在浏览器的cookie
 session_start();
 
 // session存储指定值
@@ -17,3 +17,13 @@ var_dump($_SESSION);
 //销毁
 unset($_SESSION["UID"]);
 var_dump($_SESSION);
+
+// session_start()函数可以传入一个数组，可以覆盖php.ini中session的配置.例如:
+session_start([
+  // 设置session是私有的
+  'cache_limiter'=>'private',
+  // 设置为true后在读取完会话数据之后， 立即关闭会话存储文件，不做任何修改
+  'read_and_close'=>true,
+  // SessionID在客户端Cookie储存的时间，默认是0，代表浏览器一关闭SessionID就作废
+  'cookie_lifetime'=>3600
+]);
