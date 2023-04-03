69bfd37c13eb2046f71820dd3c91116a4244651d
NoDream <1802024110@qq.com>
2023-04-03 15:51:52 +0800

    成功导入模型

--- Vue/trois_js/src/views/Div_model.vue
+++ Vue/trois_js/src/views/Div_model.vue
@@ -1,14 +1,13 @@
 <template>
   <Renderer ref="renderer" :antialias="true" :orbit-ctrl="true" :pointer="{ intersectRecursive: true }" :resize="true">
-    <Camera :position="{z:-1,x:0,y:0}"/>
-    <Scene :background="0xffcc66">
-      <PointLight :position="{y:50,z:50,x:0}"/>
-      <PointLight :position="{y:50,z:50,x:20}"/>
+    <Camera :position="{z:800}" :far="20000"/>
+    <Scene :background="0x000000">
+      <HemisphereLight color="#B1E1FF" :intensity="0.6"/>
+      <DirectionalLight  color="#FFFFFF" :intensity="0.8" :position="{x:5,y:10,z:2}"/>
       <GltfModel
           ref="model"
-          :position="{y:-1}"
-          src="src/assets/model/naxida_bailu/1.glb"
-          @click="onPointerEvent" @load="onLoad">
+          src="src/assets/model/cartoon_lowpoly_small_city_free_pack/scene.gltf"
+      @load="onLoad">
       </GltfModel>
     </Scene>
   </Renderer>
@@ -18,57 +17,34 @@
 import {onMounted, ref} from "vue";
 import {DoubleSide, Float32BufferAttribute, MeshBasicMaterial, TextureLoader} from "three";
 
-function onPointerEvent(event) {
-  // console.log(event);
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+  const localPrefix = isLast ? '└─' : '├─';
+  lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+  const newPrefix = prefix + (isLast ? '  ' : '│ ');
+  const lastNdx = obj.children.length - 1;
+  obj.children.forEach((child, ndx) => {
+    const isLast = ndx === lastNdx;
+    dumpObject(child, lines, isLast, newPrefix);
+  });
+  return lines;
 }
 
-function onLoad(object) {
-  // const textureLoader = new TextureLoader()
-  object.scene.traverse(function (child) {
-    switch (child.name) {
-      case "白露1010":
-        const geometry = child.geometry
-        const positions = geometry.attributes.position.array
-        const uvs = geometry.attributes.uv.array
-        // 偏移量
-        const offset = 1.5
-        for (let i = 0; i < uvs.length; i += 2) {
-          const u = uvs[i];
-          const v = uvs[i + 1];
-          // 将uv坐标旋转45度并偏移offset个单位
-          const uNew = (u - offset) * Math.cos(Math.PI / 4) - (v - offset) * Math.sin(Math.PI / 4) + offset;
-          const vNew = (u - offset) * Math.sin(Math.PI / 4) + (v - offset) * Math.cos(Math.PI / 4) + offset;
-          uvs[i] = uNew;
-          uvs[i + 1] = vNew;
-        }
-        geometry.setAttribute('uv', new Float32BufferAttribute(uvs, 2));
-
-        // 外套
-        const texture0 = new TextureLoader().load("src/assets/model/naxida_bailu/外套n.png")
-        child.material = new MeshBasicMaterial({map: texture0});
-        break
-      case "白露1010_1":
-        // 脸
-        const texture1 = new TextureLoader().load("src/assets/model/naxida_bailu/颜.png")
-        child.material = new MeshBasicMaterial({map: texture1});
-        break
-      case "白露1010_2":
-        // 头发
-        const texture2 = new TextureLoader().load("src/assets/model/naxida_bailu/髮2.png")
-        child.material = new MeshBasicMaterial({map: texture2, side: DoubleSide});
-        break
-      case "白露1010_3":
-        // 衣服
-        const texture3 = new TextureLoader().load("src/assets/model/naxida_bailu/衣服n.png")
-        child.material = new MeshBasicMaterial({map: texture3});
-        break
-    }
-  });
+function onLoad(object){
+  const cars = object.scene.getObjectByName('Cars')
+  // console.log(cars)
 }
 
 const model = ref(null)
 const renderer = ref(null)
 onMounted(() => {
+  renderer?.value.onBeforeRender(()=>{
+    const cars = model.value.scene.getObjectByName('Cars')
+    if (cars){
+      cars.children.forEach((car)=>{
+        car.rotation.y += 0.01
+      })
+    }
+  })
 })
 </script>
 
