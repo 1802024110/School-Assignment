167131e9afa5369c75fcd1a20fab35f227a60446
NoDream <1802024110@qq.com>
2023-04-20 07:55:43 +0800

    update

---
+++ GoLang/structure/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Books struct {
+	title   string
+	author  string
+	subject string
+	bookId  int
+}
+
+func printBook(book Books) {
+	fmt.Println("Book title : ", book.title)
+	fmt.Println("Book author : ", book.author)
+	fmt.Println("Book subject :  ", book.subject)
+	fmt.Println("Book bookId : ", book.bookId)
+}
+
+func main() {
+	var Book1 Books
+	Book1.title = "Go Programming"
+	Book1.author = "Kundalini"
+	Book1.subject = "Go Programming Tutorial"
+	Book1.bookId = 6495407
+	printBook(Book1)
+
+	var Book2 Books
+	Book2.title = "Python Programming"
+	Book2.author = "Kundalini"
+	Book2.subject = "Python Programming Tutorial"
+	Book2.bookId = 6495407
+	printBook(Book2)
+}
