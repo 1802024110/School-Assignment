294b3cdfe3213b9eec870a5f17ff2e7fced8e426
noDream <18020241110@qq.com>
2023-05-24 15:18:27 +0800

    type(修改): 学生分析文件夹移动，修改文件位置 \nbody: 将BigData文件夹下的xsjyxx.csv、xskccj.csv、xskcxx.csv、分析.docx、分析.pptx、学生就业xsjyxx.xlsx、学生课程信息xskcxx.xlsx移动到了BigData文件夹下的学生分析文件夹中，并重命名了文件位置。\nfooter: 无refactor(文件路径): 文件路径重命名\n\n将BigData下的学生课程成绩.xlsx和频数分析_SYSZDMC_DWSZDMC.docx移至BigData/学生分析文件夹下，并重命名对应文件名。\n\nfeat(新增文件): 新增iris.csv文件\n\n新增BigData/水仙花/iris.csv文件，文件内容为花萼长度、花萼宽度、花瓣长度、花瓣宽度和花种类等信息。未提供足够的信息，无法进行总结。请提供完整的git diff输出。feat(算法): 新增括号匹配算法实现\n\n实现了一个简单的括号匹配算法，可以检测不同类型的括号是否匹配，并打印出相应结果。包括不匹配、左括号多于右括号、右括号多于左括号、匹配成功等情况。

--- DataStructures/LinkStack/main.c
+++ DataStructures/LinkStack/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <windows.h>
 
-typedef int ElemType;
+typedef char ElemType;
 typedef struct SNODE {
     ElemType data;
     struct SNODE *next;
@@ -41,7 +42,7 @@ LinkStack PushStack(LinkStack top, ElemType e) {
     }
 }
 
-/** 出栈
+/** 出栈,带返回值
  *
  * */
 LinkStack PopStack(LinkStack top,ElemType *e) {
@@ -52,6 +53,21 @@ LinkStack PopStack(LinkStack top,ElemType *e) {
         LinkStack p = top;
         top = top->next;
         *e = p->data;
+        free(p);
+        return top;
+    }
+}
+
+/** 出栈,不带返回值
+ *
+ * */
+LinkStack PopStackNoReturn(LinkStack top) {
+    if (isEmpty(top)) {
+        printf("栈为空！\n");
+        return NULL;
+    } else {
+        LinkStack p = top;
+        top = top->next;
         free(p);
         return top;
     }
@@ -83,15 +99,55 @@ void Display(LinkStack top) {
     printf("\n");
 }
 
-int main() {
+// 匹配括号的函数
+int MatchBracket(){
+    // 初始化栈
     LinkStack top = Init();
-    top = PushStack(top, 40);
-    top = PushStack(top, 30);
-    top = PushStack(top, 20);
-    top = PushStack(top, 10);
-    ElemType e ;
-    top = PopStack(top,&e);
-    printf("出栈元素为：%d\n",e);
-    Display(top);
+    // 提示用户输入表达式
+    printf("请输入表达式，以换行符结束：");
+    // 逐个读取输入的字符
+    char ch;
+    while ((ch = getchar()) != '\n') {
+        // 如果是左括号，入栈
+        if (ch == '(' || ch == '[' || ch == '{') {
+            top = PushStack(top, ch);
+        }
+            // 如果是右括号，进行匹配
+        else if (ch == ')' || ch == ']' || ch == '}') {
+            // 如果栈为空，说明右括号多于左括号
+            if (isEmpty(top)) {
+                printf("右括号多于左括号！\n");
+                return 0;
+            }
+                // 如果栈不为空，进行匹配
+            else {
+                char topElem = GetTop(top);
+                // 如果匹配成功，出栈
+                if ((topElem == '(' && ch == ')') || (topElem == '[' && ch == ']') || (topElem == '{' && ch == '}')) {
+                    top = PopStackNoReturn(top);
+                }
+                    // 如果匹配失败，返回错误
+                else {
+                    printf("括号不匹配！\n");
+                    return 0;
+                }
+            }
+        }
+    }
+    // 如果栈为空，说明匹配成功
+    if (isEmpty(top)) {
+        printf("括号匹配！\n");
+        return 1;
+    }
+        // 如果栈不为空，说明左括号多于右括号
+    else {
+        printf("左括号多于右括号！\n");
+        return 0;
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+    MatchBracket();
     return 0;
 }
