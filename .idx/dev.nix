e562eb30ed8a1749d4a368945b89ab913c8d133a
im.hansiy <im.hansiy@gmail.com>
2024-09-20 06:39:39 +0000

    添加云环境

---
+++ .idx/dev.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.openjdk11
+  ];
+  services.docker.enable = true;
+  idx.extensions = [
+    "Dart-Code.dart-code"
+    "Dart-Code.flutter"
+    "zhuangtongfa.material-theme"
+    "LaurentTreguier.vscode-simple-icons"
+    "circlecodesolution.ccs-flutter-color"
+    "jeroen-meijer.pubspec-assist"
+    "Nash.awesome-flutter-snippets"
+    "robert-brunhage.flutter-riverpod-snippets"
+    "AMiner.codegeex"
+  ];
+  idx.previews = {
+    enable = true;
+    previews = { };
+  };
+  idx.workspace.onCreate = { };
+}
